ba0be9d813f6ba3ed039711552e51dbf255b9a48
Auqpiro <zzoomzoone@gmail.com>
2023-08-08 00:43:19 -0600

    change-- "Move socket listening from provider to init func"

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,5 +1,7 @@
 import { Provider } from 'react-redux';
 import store from './slices/index.js';
+import { channelsActions } from './slices/channelsSlice.js';
+import { messagesActions } from './slices/messagesSlice.js';
 import CollectorProvider from './providers/collector.jsx';
 import SocketProvider from './providers/soket.jsx';
 import AuthProvider from './providers/auth.jsx';
@@ -11,6 +13,18 @@ const init = (socket) => {
   const isDarkPrefer = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const preferTheme = isDarkPrefer ? 'dark' : 'light';
   document.documentElement.setAttribute('data-bs-theme', preferTheme);
+  socket.on('newMessage', (payload) => {
+    store.dispatch(messagesActions.addMessage(payload));
+  });
+  socket.on('newChannel', (payload) => {
+    store.dispatch(channelsActions.addChannel(payload));
+  });
+  socket.on('removeChannel', (payload) => {
+    store.dispatch(channelsActions.removeChannel(payload));
+  });
+  socket.on('renameChannel', (payload) => {
+    store.dispatch(channelsActions.renameChannel(payload));
+  });
   return (
     <CollectorProvider>
       <Provider store={store}>
--- frontend/src/providers/soket.jsx
+++ frontend/src/providers/soket.jsx
@@ -1,25 +1,6 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { SocketContext } from '../context/index.js';
-import { channelsActions } from '../slices/channelsSlice.js';
-import { messagesActions } from '../slices/messagesSlice.js';
 
 const SocketProvider = ({ socket, children }) => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    socket.on('newMessage', (payload) => {
-      dispatch(messagesActions.addMessage(payload));
-    });
-    socket.on('newChannel', (payload) => {
-      dispatch(channelsActions.addChannel(payload));
-    });
-    socket.on('removeChannel', (payload) => {
-      dispatch(channelsActions.removeChannel(payload));
-    });
-    socket.on('renameChannel', (payload) => {
-      dispatch(channelsActions.renameChannel(payload));
-    });
-  }, [dispatch, socket]);
   const clarify = (...arg) => new Promise((res, rej) => {
     socket.timeout(5000).emit(...arg, (err, response) => (response?.status === 'ok' ? res(response?.data) : rej(err)));
   });
